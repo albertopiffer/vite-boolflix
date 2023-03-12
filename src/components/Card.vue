7c9f9fb2165cbebbb008b847dadb8b7d819032cb
albertopiffer <albertopiffer96@gmail.com>
2023-03-12 19:18:01 +0100

    FEAT milestone 4

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,26 @@
 <template>
     <li class="card">
-        <h3 v-if="type == 'm'">{{ item.title }}</h3>
-        <h3 v-else>{{ item.name }}</h3>
-
-        <p v-if="type == 'm'">{{ item.original_title }}</p>
-        <p v-else>{{ item.original_name }}</p>
 
         <img :src="thumbnail" alt="">
 
-        <p ref="flagEmote"></p>
-        <!-- <p>{{ starsVoteCalc }}</p> -->
-        <div>
-            <font-awesome-icon v-for="el in starsVoteSolid()" icon="fa-solid fa-star" />
-            <font-awesome-icon v-for="el in starsVoteRegular()" icon="fa-regular fa-star" />
+        <div class="info">
+
+            <h3 v-if="type == 'm'">{{ item.title }}</h3>
+            <h3 v-else>{{ item.name }}</h3>
+
+            <p v-if="type == 'm'">{{ item.original_title }}</p>
+            <p v-else>{{ item.original_name }}</p>
+
+            <p ref="flagEmote"></p>
+            <!-- <p>{{ starsVoteCalc }}</p> -->
+            <div class="stars">
+                <font-awesome-icon v-for="el in starsVoteSolid()" icon="fa-solid fa-star" />
+                <font-awesome-icon v-for="el in starsVoteRegular()" icon="fa-regular fa-star" />
+            </div>
+            <!-- <p>{{ item.vote_average }}</p> -->
+            <p>{{ item.overview }}</p>
+
         </div>
-        <!-- <p>{{ item.vote_average }}</p> -->
-        <p>{{ item.overview }}</p>
     </li>
 </template>
 
@@ -85,14 +90,37 @@ export default {
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    gap: 10px;
 
     border: 1px dotted black;
 
-    padding: 10px;
+    height: 488px;
+    width: 342px;
 
-    // height: 550px;
-    // max-height: 550px !important;
+    max-height: 488px;
+    max-width: 342px;
+
     overflow: hidden;
+
+    position: relative;
+
+    .info {
+        background-color: black;
+        color: white;
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+
+        display: none;
+        flex-direction: column;
+        justify-content: flex-start;
+
+        gap: 20px;
+    }
+
+}
+
+.card:hover .info {
+    display: flex;
 }
 </style>
